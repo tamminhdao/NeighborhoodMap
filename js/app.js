c7383d908d604865fa83502345a215ceb1b0e5d1
Tam Dao <tamminhdaoabbas@gmail.com>
2017-02-25 15:29:04 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,136 @@
+//Store raw data loaded from 3rd party API
+//A list of SF area ice cream parlors in this case
+var loadedData = [];
+
+//Load data from Foursquare
+function foursquareCall() {
+    var foursquareUrl = "https://api.foursquare.com/v2/venues/search?ll=37.77926,-122.419265&query=icecream&client_id=POWMWFWIJYX2DYSPVDZGWUALNC4RON5ROTEPHNDZKIYOTUTR&client_secret=PHC4Z52PPQJM5SMCLNN4UAGVYW5PQIKOWX23FDQWLCVB3J3S&v=20170203";
+
+    //Handle Error
+    var requestTimeout = setTimeout (function(){
+        window.alert ("Foursquare is taking longer than usual to response.");
+    }, 5000); //wait 5 sec
+
+    $.ajax({
+        url: foursquareUrl,
+        dataType: 'jsonp',
+        success: function (response) {
+            //If ajax resquest went through, abort error alert
+            clearTimeout (requestTimeout);
+
+            var result = response.response.venues;
+            console.log (result);
+            result.forEach (function (item) {
+                loadedData.push(item);
+            })
+        }
+    });
+}
+
+
+
+foursquareCall ();
+console.log ("loadedData Length: " + loadedData.length);
+
+
+
+//Model 
+//constructor function for each ice cream parlor (i.e. Venue instance) to be place on the map
+var Venue = function (data) {
+    var self = this;
+    this.name = ko.observable(data.name);
+    this.address = ko.observable(data.location.formattedAddress);
+    this.phone = ko.observable(data.contact.formattedPhone);
+}
+
+//ViewModel constructor function
+function ViewModel () {
+    var self = this;
+
+    //Knockout Bindings for the Header
+    //assign initial visibility status for the selection icons and the option box
+    this.hamburgerIcon = ko.observable (true);
+    this.crossIcon = ko.observable (false);
+    this.optionsBox = ko.observable (false);
+
+    //show options box when click on hamburger icon, alternate between hamburger and cross icons
+    this.showOptions = function() {
+        self.hamburgerIcon(false);
+        self.crossIcon(true);
+        self.optionsBox(true);
+    };
+
+    //hide options box when click on hamburger icon, alternate between hamburger and cross icons
+    this.hideOptions = function() {
+        self.crossIcon(false);
+        self.hamburgerIcon(true);
+        self.optionsBox(false);
+    };
+
+    //Create and bind Venue instances
+    this.venueList = ko.observableArray([]);
+
+    loadedData.forEach (function (item) {
+        self.venueList.push(new Venue(item));
+    })
+}
+
+var vm = new ViewModel() 
+ko.applyBindings (vm);
+
+// Google Map
+var map;
+var markers = [];
+var defaultIcon;
+var highlightedIcon;
+
+
+function initMap() {
+    //style the map
+    var styles = [
+        {
+            featureType: 'poi',
+            elementType: 'poi.attraction',
+            stylers : [
+                { weight: 10 }
+            ]
+        },
+        {
+            featureType: 'poi',
+            elementType: 'poi.park',
+            stylers : [
+                { weight: 9 }
+            ]
+        },
+        {
+            featureType: 'road',
+            elementType: 'road.highway',
+            stylers : [
+                { visibility: 'simplified' }
+            ]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+        },
+        {
+            featureType: 'administrative',
+            elementType: 'administrative.neighborhood',
+            stylers : [
+                { visibility: 'off' }
+            ]
+        }
+    ];
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.77733, lng: -122.441415}, //Panhandle
+        //center: {lat: 37.77926, lng: -122.419265}, //City Hall
+        zoom: 14,
+        styles: styles,
+        mapTypeControl: false
+    });
+}
+
+
